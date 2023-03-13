6390ca0c06ab61016d394fb2c02a08a9a07ce943
28984632406ana <huseyinkayhan@anadolu.edu.tr>
2023-03-13 17:26:48 +0300

    Workflows.js

--- src/pages/Content.js
+++ src/pages/Content.js
@@ -2,9 +2,6 @@ import React from 'react';
 import Sidebar from "../components/SideBar";
 import NavBar from "../components/NavBar";
 import {Route, Routes, Link, NavLink, Outlet} from "react-router-dom";
-import Home from "../views/Home";
-import AssignStuff from "../views/Assign/AssignStuff";
-import SpecialAppend from "../views/Assign/SpecialAppend";
 
 function Content(props) {
     return (
--- src/views/workflows/Workflows.js
+++ src/views/workflows/Workflows.js
@@ -8,7 +8,8 @@ function Workflows(props) {
     const [workFlowsDB, setWorkFlowsDB] = useState([])
     const [relationsDB, setRelationsDB] = useState([])
     const [workFlowNames, setWorkFlowNames] = useState([])
-    const [selectedWorkFlowID, setSelectedWorkFlowID]=useState([])
+    const [selectedWorkFlowID, setSelectedWorkFlowID] = useState([])
+    const [selectedWorkFlowSteps, setSelectedWorkFlowSteps] = useState([])
 
     useEffect(() => {
         getRelationsFromDB()
@@ -16,10 +17,15 @@ function Workflows(props) {
     }, [])
 
     useEffect(() => {
-        filterWorkFlowsName()
+        console.log("workFlowsDB", workFlowsDB);
+        console.log("relationsDB", relationsDB);
+
+        setWorkFlowNames(filterWorkFlowsStepsWithParents(0))
     }, [workFlowsDB, relationsDB])
 
-
+    useEffect(() => {
+        setSelectedWorkFlowSteps(filterWorkFlowsStepsWithParents(selectedWorkFlowID))
+    }, [selectedWorkFlowID])
     const createNewWorkFlow = () => {
 
         fireBaseApi().post('/workflows.json', {"flowName": newFlowName},
@@ -33,8 +39,19 @@ function Workflows(props) {
             .catch(err => console.log(err));
     }
 
+    const addNewStep = () => {
+        fireBaseApi().post('/workflows.json', {"flowName": newFlowName},
+
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(res => stepRelationships(res.data.name, [selectedWorkFlowID]))
+            .catch(err => console.log(err));
+    }
     const stepRelationships = (id, parents) => {
-        console.log(parents);
+        console.log("stepRelationships",parents);
         parents.forEach(p => {
             console.log(p);
             fireBaseApi().post('/stepRelationships.json', {"childID": id, "parentID": p},
@@ -67,65 +84,77 @@ function Workflows(props) {
             .catch(err => console.log(err))
     }
 
-    const filterWorkFlowsName = () => {
+    const filterWorkFlowsStepsWithParents = (parentID) => {
         let workFlowNamesArr = []
         for (const key in relationsDB) {
             console.log(relationsDB[key]);
-            if (relationsDB[key].parentID === 0) {
+            if (relationsDB[key].parentID === parentID) {
 
                 let wf = workFlowsDB[relationsDB[key].childID]
 
                 console.log(wf);
 
-                workFlowNamesArr.push({"ID":relationsDB[key].childID ,...wf})
+                workFlowNamesArr.push({"ID": relationsDB[key].childID, ...wf})
                 // console.log(relationsDB[key]);
             }
         }
-        console.log(workFlowNamesArr);
-        setWorkFlowNames(workFlowNamesArr)
+        // console.log(workFlowNamesArr);
+        return workFlowNamesArr
     }
 
 
-    return (<div>
+    return (
+        <div>
 
-        <div>Akış şablonları</div>
-        <div className={"d-flex flex-row-reverse pe-5"}>
-            <button onClick={() => setNewFlowVisible(true)} className={"btn btn-success"}>Yeni Akış Oluştur
-            </button>
+            <div>Akış şablonları</div>
+            <div className={"d-flex flex-row-reverse pe-5"}>
+                <button onClick={() => setNewFlowVisible(true)} className={"btn btn-success"}>Yeni Akış Oluştur
+                </button>
 
-        </div>
-        {newFlowVisible &&
-            <div className={"d-flex mt-3 mb-3 pe-5"}>
-            <div className="input-group me-2 ">
-                <span className="input-group-text" id="inputGroup-sizing-default">Akış Adı</span>
-                <input onChange={e => setNewFlowName(e.target.value)} type="text" className="form-control"
-                       aria-label="Sizing example input"
-                       aria-describedby="inputGroup-sizing-default"/>
             </div>
-            <button onClick={() => createNewWorkFlow()} className={"btn btn-success"}>Ekle</button>
+            {newFlowVisible &&
+                <div className={"d-flex mt-3 mb-3 pe-5"}>
+                    <div className="input-group me-2 ">
+                        <input onChange={e => setNewFlowName(e.target.value)} placeholder={"Akış Adı"} type="text"
+                               className="form-control"
+                               aria-label="Sizing example input"
+                               aria-describedby="inputGroup-sizing-default"/>
+                    </div>
+                    <button onClick={() => createNewWorkFlow()} className={"btn btn-success"}>Ekle</button>
 
-        </div>
-        }
+                </div>
+            }
 
 
-        <h4 className="mt-4">Kayıtlı Akışlar</h4>
-        <hr/>
+            <h4 className="mt-4">Kayıtlı Akışlar</h4>
+            <hr/>
             {workFlowNames.map((w, i) =>
-                <div key={i} onClick={()=>setSelectedWorkFlowID(w.ID)}>
-                    {w.flowName}
+                <div key={i} onClick={() => setSelectedWorkFlowID(w.ID)}>
+                    <div
+                        className={"border p-2 mb-2 rounded pointer bg-white ps-3 fw-bold text-uppercase"}> {w.flowName}</div>
                     {
-                        selectedWorkFlowID===w.ID&&
-                        <div className={"d-flex mt-3 mb-3 pe-5"}>
-                            <div className="input-group me-2 ">
-                                <input onChange={e => setNewFlowName(e.target.value)} type="text" className="form-control"
-                                       aria-label="Sizing example input"
-                                       aria-describedby="inputGroup-sizing-default"
-                                        defaultValue={"Yeni Adım"}
-                                />
-                            </div>
-                            <button onClick={() => createNewWorkFlow()} className={"btn btn-success"}>Yeni Adım Ekle</button>
+                        selectedWorkFlowID === w.ID && <>
+                            {selectedWorkFlowSteps.map((s, i) =>
+                                <div className={"border d-flex mb-2 mx-5 p-2 ps-3 rounded bg-white"}>
+                                    {s.flowName}
+                                </div>
+                            )}
+                            <div className={"d-flex mt-3 mb-5 px-5"}>
 
-                        </div>
+                                <div className="input-group me-2 ">
+                                    <input onChange={e => setNewFlowName(e.target.value)} type="text"
+                                           className="form-control"
+                                           aria-label="Sizing example input"
+                                           aria-describedby="inputGroup-sizing-default"
+                                           placeholder={"Yeni Adım"}
+                                    />
+                                </div>
+                                <button onClick={() => addNewStep()} className={"btn btn-success"}>Ekle</button>
+
+                            </div>
+                            <hr/>
+                        </>
+
                     }
                     <div>
 
@@ -135,7 +164,7 @@ function Workflows(props) {
             )}
 
 
-    </div>);
+        </div>);
 }
 
 export default Workflows;
